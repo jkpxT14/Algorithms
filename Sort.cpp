642277f8ebca7f208e6a1a966865239f67f937aa
jkpxT14 <jkpxtfin@gmail.com>
2024-03-09 16:47:15 +0900

    Commit

---
+++ Sort.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+using ll=long long;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+}
