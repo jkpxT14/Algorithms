c147281e7206eeead8998131615e91e80272324a
jkpxT14 <jkpxtfin@gmail.com>
2024-03-18 19:46:35 +0900

    Commit

--- Dijkstra.cpp
+++ Dijkstra.cpp
@@ -12,5 +12,16 @@ int main()
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    vector<vector<int>> v;
+    int V, E, K;
+    vector<vector<int>> weight(20001, vector<int> (20001, -1));
+    vector<int> dist(20001, -1);
+    vector<bool> visit(20001, false);
+    cin>>V>>E;
+    cin>>K;
+    for(int i=1; i<=E; i++){
+        int u, v, w;
+        cin>>u>>v>>w;
+        weight[u][v]=w;
+        weight[v][u]=w;
+    }
 }
