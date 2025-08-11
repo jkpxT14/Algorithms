17bbbf86aa95007b50d891b2079e418a84decc9a
jkpxT14 <jkpxtfin@gmail.com>
2025-08-11 18:24:43 +0900

    Commit

---
+++ A.cpp
@@ -0,0 +1,32 @@
+// BOJ 14215
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <stack>
+#include <queue>
+#include <tuple>
+#include <utility>
+// #include <limits>
+
+using namespace std;
+
+using ll=long long;
+using vi=vector<int>;
+using vb=vector<bool>;
+using si=stack<int>;
+using qi=queue<int>;
+using pii=pair<int, int>;
+using vpii=vector<pair<int, int>>;
+using vvpii=vector<vector<pair<int, int>>>;
+// #define INT_MAX 2147483647
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    // cout.tie(nullptr);
+
+    return 0;
+}
