9cc8a4c38a76e6d37968874896411b72bd6197eb
jkpxT14 <jkpxtfin@gmail.com>
2025-08-12 20:06:15 +0900

    Commit

---
+++ Bellman-Ford.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <algorithm>
+#include <utility>
+#include <tuple>
+// #include <limits>
+
+using namespace std;
+
+using ll=long long;
+using vi=vector<int>;
+using vb=vector<bool>;
+using si=stack<int>;
+using qi=queue<int>;
+using dqi=deque<int>;
+using pii=pair<int, int>;
+using vpii=vector<pair<int, int>>;
+using dqpii=deque<pair<int, int>>;
+using vvpii=vector<vector<pair<int, int>>>;
+
+// #define INT_MAX 2147483647
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    // cout.tie(nullptr);
+
+    int N, M;
+    cin>>N>>M;
+    for(int i=0; i<M; i++){
+        int A, B, C;
+        cin>>A>>B>>C;
+    }
+
+    return 0;
+}
