aa3ba12c3405d2ccaea151b553ba507eea3f1e0d
jkpxT14 <jkpxtfin@gmail.com>
2025-08-12 22:34:14 +0900

    Commit

--- B.cpp
+++ B.cpp
@@ -26,11 +26,17 @@ using vvpii=vector<vector<pair<int, int>>>;
 #define INT_MAX 2147483647
 const int INF=INT_MAX;
 
+// ans = answer
+// cur = current
+
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     // cout.tie(nullptr);
+
+    int N=2e4;
+    cout<<N;
 
     return 0;
 }
